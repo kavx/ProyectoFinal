e3df0ca36002abce31ea440f92e90b1ed1a41ef8
kavx <kavx1001@gmail.com>
2016-04-16 03:32:15 -0600

    Configuando
    Configurando Navigation Drawer

--- GestionUniversitaria/app/src/main/java/com/example/kavxpc/gestionuniversitaria/preLoginAtivity.java
+++ GestionUniversitaria/app/src/main/java/com/example/kavxpc/gestionuniversitaria/preLoginAtivity.java
@@ -1,5 +1,6 @@
 package com.example.kavxpc.gestionuniversitaria;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,8 +81,9 @@ public class preLoginAtivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
+        if (id == R.id.nav_login) {
+
+
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
