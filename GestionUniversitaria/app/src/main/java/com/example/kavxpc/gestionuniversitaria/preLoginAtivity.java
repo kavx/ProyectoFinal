78d4c625c6a3f9699af4d70a5b4d29d5420f013e
kavx <kavx1001@gmail.com>
2016-04-16 01:33:46 -0600

    Probando

--- GestionUniversitaria/app/src/main/java/com/example/kavxpc/gestionuniversitaria/preLoginAtivity.java
+++ GestionUniversitaria/app/src/main/java/com/example/kavxpc/gestionuniversitaria/preLoginAtivity.java
@@ -19,7 +19,7 @@ public class preLoginAtivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pre_login_ativity);
+        setContentView(R.layout.main_navbar);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
